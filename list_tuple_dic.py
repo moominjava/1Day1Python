1c94c5912608893143b284371e8bb8e7fbe04821
moominjava <moominjava@gmail.com>
2020-07-14 21:44:32 +0900

    Add *

---
+++ list_tuple_dic.py
@@ -0,0 +1,22 @@
+python basic
+
+파이선에서 true는 True, False로 표현(대문자!)
+
+list / dic / tuple 
+[] / {} / ()
+
+r = range(0, 20, 2)
+>>r
+range(0, 20, 2)
+11 in r
+>>False
+10 in r
+>>True
+r.index(10)
+>>5
+r[5]
+>>10
+r[:5]
+>>range(0, 10, 2)
+r[-1]
+>>18
